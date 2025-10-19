b3d52f2d5294ca84883c9e67515cc7d947793c94
aditya-kr007 <adityakr.mnnit@gmail.com>
2025-10-20 02:45:25 +0530

    middleware

---
+++ backend/middleware/VerifyJwtToken.js
@@ -0,0 +1,18 @@
+const jwt=require("jsonwebtoken")
+
+const VerifyJwtToken=(req,res,next)=>{
+    const authHeader=req.headers['authorization']
+    const token=authHeader && authHeader.split(' ')[1]
+    if(token==null){
+        return res.status(401).json({error:"No token provided"})
+    }
+    jwt.verify(token,process.env.JWT_SECRET_KEY,(err,user)=>{
+        if(err){
+            return res.status(403).json({error:"Invalid token"})
+        }
+        req.user=user.id
+        next()
+    })
+}
+
+module.exports={VerifyJwtToken}
